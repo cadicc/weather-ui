44c204e3ca4ab6cb42109a2257604397d6b40bc1
Viet Trung <vjetrung2@gmail.com>
2022-05-31 19:48:09 +0700

    add library

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <div className="navBar">
+            <ul>
+                <li>Nav icon</li>
+                <li>Home</li>
+                <li>Historical Weather</li>
+                <li>Favorites</li>
+                <li>Feedback</li>
+            </ul>
+        </div>
+    );
+};
+export default NavBar;
